8328af4bcad3f18321129463f445854f53aa90bb
Hermann Hueck <hermann.hueck@mailbox.org>
2022-12-28 18:12:51 +0100

    build: start version identifiers with lowercase characters

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,25 +4,25 @@ object Dependencies {
 
   import Versions._
 
-  lazy val http4sBlazeServer = "org.http4s"      %% "http4s-blaze-server" % Http4sVersion
-  lazy val http4sEmberServer = "org.http4s"      %% "http4s-ember-server" % Http4sVersion
-  lazy val http4sEmberClient = "org.http4s"      %% "http4s-ember-client" % Http4sVersion
-  lazy val http4sBlazeClient = "org.http4s"      %% "http4s-blaze-client" % Http4sVersion
-  lazy val http4sDsl         = "org.http4s"      %% "http4s-dsl"          % Http4sVersion
-  lazy val http4sCirce       = "org.http4s"      %% "http4s-circe"        % Http4sVersion
-  lazy val circeGeneric      = "io.circe"        %% "circe-generic"       % CirceVersion
-  lazy val circeLiteral      = "io.circe"        %% "circe-literal"       % CirceVersion
-  lazy val circeCore         = "io.circe"        %% "circe-core"          % CirceVersion
-  lazy val circeParser       = "io.circe"        %% "circe-parser"        % CirceVersion
-  lazy val munit             = "org.scalameta"   %% "munit"               % MunitVersion
-  lazy val scalaCheck        = "org.scalacheck"  %% "scalacheck"          % ScalaCheckVersion
-  lazy val munitCE3          = "org.typelevel"   %% "munit-cats-effect-3" % MunitCE3Version
-  // lazy val logback           = "ch.qos.logback"   % "logback-classic"     % LogbackVersion
-  lazy val reactormonk       = "org.reactormonk" %% "cryptobits"          % ReaktormonkVersion
-  lazy val swaggerUI         = "org.webjars"      % "swagger-ui"          % SwaggerUIVersion
+  lazy val http4sBlazeServer = "org.http4s"      %% "http4s-blaze-server" % http4sVersion
+  lazy val http4sEmberServer = "org.http4s"      %% "http4s-ember-server" % http4sVersion
+  lazy val http4sEmberClient = "org.http4s"      %% "http4s-ember-client" % http4sVersion
+  lazy val http4sBlazeClient = "org.http4s"      %% "http4s-blaze-client" % http4sVersion
+  lazy val http4sDsl         = "org.http4s"      %% "http4s-dsl"          % http4sVersion
+  lazy val http4sCirce       = "org.http4s"      %% "http4s-circe"        % http4sVersion
+  lazy val circeGeneric      = "io.circe"        %% "circe-generic"       % circeVersion
+  lazy val circeLiteral      = "io.circe"        %% "circe-literal"       % circeVersion
+  lazy val circeCore         = "io.circe"        %% "circe-core"          % circeVersion
+  lazy val circeParser       = "io.circe"        %% "circe-parser"        % circeVersion
+  lazy val munit             = "org.scalameta"   %% "munit"               % munitVersion
+  lazy val scalaCheck        = "org.scalacheck"  %% "scalacheck"          % scalaCheckVersion
+  lazy val munitCE3          = "org.typelevel"   %% "munit-cats-effect-3" % munitCE3Version
+  // lazy val logback           = "ch.qos.logback"   % "logback-classic"     % logbackVersion
+  lazy val reactormonk       = "org.reactormonk" %% "cryptobits"          % reaktormonkVersion
+  lazy val swaggerUI         = "org.webjars"      % "swagger-ui"          % swaggerUIVersion
   // lazy val jcdp              = "com.diogonunes"   % "JCDP"                % "2.0.3.1"
-  lazy val slf4jApi          = "org.slf4j"        % "slf4j-api"           % Slf4jVersion
-  lazy val slf4jSimple       = "org.slf4j"        % "slf4j-simple"        % Slf4jVersion
+  lazy val slf4jApi          = "org.slf4j"        % "slf4j-api"           % slf4jVersion
+  lazy val slf4jSimple       = "org.slf4j"        % "slf4j-simple"        % slf4jVersion
 
   // https://github.com/typelevel/kind-projector
   lazy val kindProjectorPlugin    = compilerPlugin(
--- project/Versions.scala
+++ project/Versions.scala
@@ -7,17 +7,17 @@ object Versions {
   val scala2Version = "2.13.9"
 
   // 3rd party libraries
-  val Http4sVersion      = "1.0.0-M37"
-  val CirceVersion       = "0.15.0-M1"
-  // val Http4sVersion     = "0.23.14"
-  // val CirceVersion      = "0.14.2"
-  val MunitVersion       = "0.7.29"
-  val MunitCE3Version    = "1.0.7"
-  val ScalaCheckVersion  = "1.17.0"
-  val LogbackVersion     = "1.4.5"
-  val ReaktormonkVersion = "1.3.1"
-  val SwaggerUIVersion   = "4.11.1"
-  val Slf4jVersion       = "2.0.6"
+  val http4sVersion      = "1.0.0-M37"
+  val circeVersion       = "0.15.0-M1"
+  // val http4sVersion     = "0.23.14"
+  // val circeVersion      = "0.14.2"
+  val munitVersion       = "0.7.29"
+  val munitCE3Version    = "1.0.7"
+  val scalaCheckVersion  = "1.17.0"
+  val logbackVersion     = "1.4.5"
+  val reaktormonkVersion = "1.3.1"
+  val swaggerUIVersion   = "4.11.1"
+  val slf4jVersion       = "2.0.6"
 
   // compiler plugins
   val kindProjectorVersion    = "0.13.2"
